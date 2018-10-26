fddb606b10e85d6191096f218ecabe6f75dab255
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2018-10-26 11:09:45 +0200

    Added more text to the file

--- DOMQuiz/script.js
+++ DOMQuiz/script.js
@@ -1,15 +1,15 @@
-var falseBtn = document.getElementById("false");
-var trueBtn = document.getElementById("true");
-var btn = document.getElementById("btn");
+var falseBtn = document.getElementById("false"); // Import the button from HTML.
+var trueBtn = document.getElementById("true"); // Import the button from HTML.
+var btn = document.getElementById("btn"); // Import the button from HTML.
 var pageCounter = 1; // starts with 1 question.
 var cointainer = document.getElementById("container");
-var data;
-var correctAnswer;
-var score = 0;
-var questionCount = 0;
+var data; // This variable containts the whole data from the API and the information given by the API.
+var correctAnswer; // This variable contain the correct answer from the question. 
+var score = 0; // This variable is just the scoreboard.
+var questionCount = 0; // This variable remember how many question is done.
 // When you push button, you get 1 question. For each push, you'll get another question.
 btn.addEventListener("click", function() {
-
+// The pagecounter just adds 1 each time button is pushed.
   var request = new XMLHttpRequest();
   request.open('GET', 'https://opentdb.com/api.php?amount=' + pageCounter + '&category=15&difficulty=easy&type=boolean', true);
   request.onreadystatechange = function () {
@@ -23,51 +23,56 @@ btn.addEventListener("click", function() {
      container.innerHTML = data.question; 
      correctAnswer = data.correct_answer;
      questionCount ++
-     trueBtn.style.visibility = "Visible";
-     falseBtn.style.visibility = "Visible";
+     trueBtn.style.visibility = "Visible"; // Makes the button visible, because it is hidden as default.
+     falseBtn.style.visibility = "Visible"; // So as soon as the question-button is pushed, the true/false-button become visible.
    });
  } 
  else {
-   console.log('error');
+   console.log('error'); // Just logs error if it recives a fault.
  }
 }
 request.responseType = 'json';
 request.send();
 });
 
-
+// True button. 
 trueBtn.addEventListener("click", function() {
-      
+      //Checks if the answer is correct.
       if(correctAnswer == "True") {
-            alert("Correct!")
+            alert("Correct!") // Alert if the answer is correct.
             score++
             console.log(score)
       } else {
-            alert("Wrong!")
+            alert("Wrong!") // Alert if the answer is false.
       }
-
+      
+      // Keep count on the amount is pushed and how many question the user should be given.
+      // and when the count is up to 5, the website will give you the amount of correct and it will reload after pushed OK.
       if (questionCount === 5) {
             alert("Correct Answers: " + score + " out of " + questionCount)
             location.reload();
       }
-      trueBtn.style.visibility = "Hidden";
+      trueBtn.style.visibility = "Hidden"; // Make the button disapear after getting pushed by the user.
      falseBtn.style.visibility = "Hidden";
 });
 
+// False button.
 falseBtn.addEventListener("click", function() {
-      
+      //Checks if the answer is correct.
       if(correctAnswer == "False") {
-            alert("Correct!")
+            alert("Correct!") // Alert if the answer is correct.
             score++
             console.log(score)
       } else {
-            alert("Wrong!")
+            alert("Wrong!") // Alert if the answer is false.
       }
 
+      // Keep count on the amount is pushed and how many question the user should be given.
+      // and when the count is up to 5, the website will give you the amount of correct and it will reload after pushed OK.
       if (questionCount === 5) {
             alert("Correct Answers: " + score + " out of " + questionCount)
             location.reload();
       }
-      trueBtn.style.visibility = "Hidden";
+      trueBtn.style.visibility = "Hidden"; // Make the button disapear after getting pushed by the user.
       falseBtn.style.visibility = "Hidden";
 });
